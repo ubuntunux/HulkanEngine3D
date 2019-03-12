af5edeb38a3d33b7cba902bb90a93e1cfb40cbe6
ubuntunux <ubuntunux@gmail.com>
2019-03-13 03:52:50 +0900

    test

--- Main.hs
+++ Main.hs
@@ -1,53 +1,42 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DataKinds #-}
 module Main (main) where
 
+import Control.Exception
+import Control.Monad
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
+import Foreign.Storable
+import Graphics.UI.GLFW (ClientAPI (..), WindowHint (..))
+import qualified Graphics.UI.GLFW as GLFW
 import Graphics.Vulkan
 import Graphics.Vulkan.Core_1_0
-import Foreign.Storable
-import Foreign.Marshal.Alloc
-import Foreign.C.String
-
-main :: IO ()
-main = do
-    res <- withVulkanInstance $ \vkInstance -> do
-      putStrLn $ "Success! " ++ show vkInstance
-      pure VK_SUCCESS
-    print res
+import Lib.Utils
+import Lib.Vulkan (withVulkanInstance)
 
 
-withVulkanInstance::(VkInstance -> IO VkResult) -> IO VkResult
-withVulkanInstance action = 
-  withCString "01-CreateInstance" $ \programNamePtr ->
-  withCString "My Perfect Haskell Engine" $ \engineNamePtr -> do
-    appInfo <- newVkData $ \appInfoPtr -> do
-      writeField @"engineVersion" appInfoPtr (_VK_MAKE_VERSION 1 0 0)
-      writeField @"pEngineName" appInfoPtr engineNamePtr
-      writeField @"applicationVersion" appInfoPtr (_VK_MAKE_VERSION 1 0 0)
-      writeField @"pApplicationName" appInfoPtr programNamePtr
-      writeField @"pNext" appInfoPtr VK_NULL_HANDLE
-      writeField @"sType" appInfoPtr VK_STRUCTURE_TYPE_APPLICATION_INFO
-      writeField @"apiVersion" appInfoPtr (_VK_MAKE_VERSION 1 0 68)
+someAcion window = do
+  print window
+  return ()
 
-    iCreateInfo <- newVkData $ \iCreateInfoPtr -> do
-      writeField @"pApplicationInfo" iCreateInfoPtr (unsafePtr appInfo)
-      writeField @"ppEnabledExtensionNames" iCreateInfoPtr VK_NULL_HANDLE
-      writeField @"enabledExtensionCount" iCreateInfoPtr 0
-      writeField @"ppEnabledLayerNames" iCreateInfoPtr VK_NULL_HANDLE
-      writeField @"enabledLayerCount" iCreateInfoPtr 0
-      writeField @"flags" iCreateInfoPtr 0
-      writeField @"pNext" iCreateInfoPtr VK_NULL_HANDLE
-      writeField @"sType" iCreateInfoPtr VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
+main::IO()
+main = do 
+  window <- withGLFWWindow $ someAcion
+  GLFW.terminate >> putStrLn "Terminated GLFW."
+  return ()
+
+
+withGLFWWindow action = do
+  GLFW.init >>= (\result -> flip unless (throwVKMsg "Failed to initialize GLFW.") result)
+  putStrLn "Initialized GLFW."
+  GLFW.getVersionString >>= (\result -> mapM_ (putStrLn . ("GLFW Version: " ++)) result)
+  GLFW.vulkanSupported >>= flip unless (throwVKMsg "GLFW reports that vulkan is not supported!")
+  GLFW.windowHint $ WindowHint'ClientAPI ClientAPI'NoAPI
+  GLFW.windowHint $ WindowHint'Resizable True
+  window <- GLFW.createWindow 800 600 "Vulkan Window" Nothing Nothing    
+  case window of
+    Nothing -> throwVKMsg "Failed to initialize GLFW window."
+    Just w -> do
+      putStrLn "Initialized GLFW window."
+      finally (action w)
+        (GLFW.destroyWindow w >> putStrLn "Closed GLFW window.")
     
-    (vkResult, vkInstance) <- alloca $ \vkInstPtr -> do
-      createInst <- vkGetInstanceProc @VkCreateInstance VK_NULL
-      vkRes <- createInst (unsafePtr iCreateInfo) VK_NULL_HANDLE vkInstPtr
-      vkI <- peek vkInstPtr
-      return (vkRes, vkI)
-    
-    if vkResult == VK_SUCCESS then do
-      r <- action vkInstance
-      vkDestroyInstance vkInstance VK_NULL_HANDLE
-      pure r
-    else
-      pure vkResult
+
