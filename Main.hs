cd5fdab6fa2e2f690eaba67b1bb3b759853b9c27
ubuntunux <ubuntunux@gmail.com>
2019-04-05 03:14:58 +0900

    update

--- Main.hs
+++ Main.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE Strict           #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main (main) where
 
 import Control.Exception
@@ -9,14 +14,54 @@ import Graphics.UI.GLFW (ClientAPI (..), WindowHint (..))
 import qualified Graphics.UI.GLFW as GLFW
 import Graphics.Vulkan
 import Graphics.Vulkan.Core_1_0
+--import Graphics.Vulkan.Ext.VK_KHR_surface
+--import Graphics.Vulkan.Ext.VK_KHR_swapchain
+import Graphics.Vulkan.Marshal.Create
 import Lib.Utils
-import Lib.Vulkan (withVulkanInstance)
+import Lib.Vulkan
 
-
---createVulkanInstance::String -> String -> [CString] -> [String] -> IO VkInstance
+glfwMainLoop :: GLFW.Window -> IO () -> IO ()
+glfwMainLoop window action = go
+  where
+    go = do
+      should <- GLFW.windowShouldClose window
+      unless should $ GLFW.pollEvents >> action >> go
+      
+createVulkanInstance::String -> String -> [CString] -> [String] -> IO VkInstance
 createVulkanInstance progName engineName extensions layers = do
- return ()
+  vkInstance <- alloca createAction
+  touchVkData iCreateInfo
+  return vkInstance
+  
+  where
+    createAction vkInstPtr = do
+      throwingVK "vkCreateInstance: Failed to create vkInstance."
+        $ vkCreateInstance (unsafePtr iCreateInfo) VK_NULL vkInstPtr    
+      peek vkInstPtr
 
+    appInfo = createVk @VkApplicationInfo
+      $ set @"sType" VK_STRUCTURE_TYPE_APPLICATION_INFO
+      &* set @"pNext" VK_NULL
+      &* setStrRef @"pApplicationName" progName
+      &* set @"applicationVersion" (_VK_MAKE_VERSION 1 0 0)
+      &* setStrRef @"pEngineName" engineName
+      &* set @"engineVersion" (_VK_MAKE_VERSION 1 0 0)
+      &* set @"apiVersion" (_VK_MAKE_VERSION 1 0 68)
+      
+    iCreateInfo = createVk @VkInstanceCreateInfo
+      $ set @"sType" VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
+      &* set @"pNext" VK_NULL
+      &* setVkRef @"pApplicationInfo" appInfo
+      &* set @"enabledLayerCount" (fromIntegral $ length layers)
+      &* setStrListRef @"ppEnabledLayerNames" layers
+      &* set @"enabledExtensionCount" (fromIntegral $ length extensions)
+      &* setListRef @"ppEnabledExtensionNames" extensions
+
+destroyVulkanInstance :: VkInstance -> IO ()
+destroyVulkanInstance vkInstance = vkDestroyInstance vkInstance VK_NULL
+
+someAcion2 vulkanInstance window = do
+  glfwMainLoop window (return ())
 
 someAcion window = do
   glfwReqExts <- GLFW.getRequiredInstanceExtensions
@@ -26,7 +71,7 @@ someAcion window = do
     extensions = glfwReqExts
     layers = ["VK_LAYER_LUNARG_standard_validation"]
   vkInstance <- createVulkanInstance progName engineName extensions layers
-  print vkInstance
+  finally (someAcion2 vkInstance window) $ destroyVulkanInstance vkInstance
   return ()
 
 main::IO()
@@ -42,7 +87,7 @@ main = do
 
 withGLFWWindow::IO (Maybe GLFW.Window)
 withGLFWWindow = do
-  GLFW.init >>= flip unless (throwVKMsg "Failed to initialize GLFW.")
+  GLFW.init >>= flip unless (throwVKMsg "Failed to initialize GLFW.")  
   putStrLn "Initialized GLFW."
   version <- GLFW.getVersionString
   mapM_ (putStrLn . ("GLFW Version: " ++)) version
