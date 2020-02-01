340ca3e1e9bf1b60c9c84469adbaf5243f29cd05
ubuntunux <ubuntunux@gmail.com>
2020-02-02 02:05:54 +0900

    update

--- Library/Vulkan/RenderPass.hs
+++ Library/Vulkan/RenderPass.hs
@@ -165,8 +165,8 @@ createPipelineLayout device = do
 
 destroyPipelineLayout :: VkDevice -> VkPipelineLayout -> IO ()
 destroyPipelineLayout device pipelineLayout = do
-  logInfo "Destroy PipelineLayout"
-  vkDestroyPipelineLayout device pipelineLayout VK_NULL
+    logInfo "Destroy PipelineLayout"
+    vkDestroyPipelineLayout device pipelineLayout VK_NULL
 
 
 createGraphicsPipeline :: VkDevice
@@ -176,146 +176,145 @@ createGraphicsPipeline :: VkDevice
                        -> String
                        -> IO GraphicsPipelineData
 createGraphicsPipeline device renderPass imageExtent vertexShaderFile fragmentShaderFile = do
-  vertexShaderCreateInfo <- createShaderStageCreateInfo device vertexShaderFile VK_SHADER_STAGE_VERTEX_BIT
-  fragmentShaderCreateInfo <- createShaderStageCreateInfo device fragmentShaderFile VK_SHADER_STAGE_FRAGMENT_BIT
-  let shaderStageInfos = [vertexShaderCreateInfo, fragmentShaderCreateInfo]
-  pipelineLayout <- createPipelineLayout device
-  let
-    vertexInputInfo :: VkPipelineVertexInputStateCreateInfo
-    vertexInputInfo = createVk @VkPipelineVertexInputStateCreateInfo
-      $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"vertexBindingDescriptionCount" 0
-      &* set @"pVertexBindingDescriptions" VK_NULL
-      &* set @"vertexAttributeDescriptionCount" 0
-      &* set @"pVertexAttributeDescriptions" VK_NULL
+    vertexShaderCreateInfo <- createShaderStageCreateInfo device vertexShaderFile VK_SHADER_STAGE_VERTEX_BIT
+    fragmentShaderCreateInfo <- createShaderStageCreateInfo device fragmentShaderFile VK_SHADER_STAGE_FRAGMENT_BIT
+    pipelineLayout <- createPipelineLayout device
+    let shaderStageInfos = [vertexShaderCreateInfo, fragmentShaderCreateInfo]
+    let vertexInputInfo :: VkPipelineVertexInputStateCreateInfo
+        vertexInputInfo = createVk @VkPipelineVertexInputStateCreateInfo
+            $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"vertexBindingDescriptionCount" 0
+            &* set @"pVertexBindingDescriptions" VK_NULL
+            &* set @"vertexAttributeDescriptionCount" 0
+            &* set @"pVertexAttributeDescriptions" VK_NULL
 
-    inputAssembly :: VkPipelineInputAssemblyStateCreateInfo
-    inputAssembly = createVk @VkPipelineInputAssemblyStateCreateInfo
-      $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"topology" VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST
-      &* set @"primitiveRestartEnable" VK_FALSE
+        inputAssembly :: VkPipelineInputAssemblyStateCreateInfo
+        inputAssembly = createVk @VkPipelineInputAssemblyStateCreateInfo
+            $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"topology" VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST
+            &* set @"primitiveRestartEnable" VK_FALSE
 
-    viewPort :: VkViewport
-    viewPort = createVk @VkViewport
-      $  set @"x" 0
-      &* set @"y" 0
-      &* set @"width" (fromIntegral $ getField @"width" imageExtent)
-      &* set @"height" (fromIntegral $ getField @"height" imageExtent)
-      &* set @"minDepth" 0
-      &* set @"maxDepth" 1
+        viewPort :: VkViewport
+        viewPort = createVk @VkViewport
+            $  set @"x" 0
+            &* set @"y" 0
+            &* set @"width" (fromIntegral $ getField @"width" imageExtent)
+            &* set @"height" (fromIntegral $ getField @"height" imageExtent)
+            &* set @"minDepth" 0
+            &* set @"maxDepth" 1
 
-    scissorRect :: VkRect2D
-    scissorRect = createVk @VkRect2D
-      $  set   @"extent" imageExtent
-      &* setVk @"offset" ( set @"x" 0 &* set @"y" 0 )
+        scissorRect :: VkRect2D
+        scissorRect = createVk @VkRect2D
+            $  set   @"extent" imageExtent
+            &* setVk @"offset" ( set @"x" 0 &* set @"y" 0 )
 
-    viewPortState :: VkPipelineViewportStateCreateInfo
-    viewPortState = createVk @VkPipelineViewportStateCreateInfo
-      $ set @"sType" VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"viewportCount" 1
-      &* setVkRef @"pViewports" viewPort
-      &* set @"scissorCount" 1
-      &* setVkRef @"pScissors" scissorRect
+        viewPortState :: VkPipelineViewportStateCreateInfo
+        viewPortState = createVk @VkPipelineViewportStateCreateInfo
+            $ set @"sType" VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"viewportCount" 1
+            &* setVkRef @"pViewports" viewPort
+            &* set @"scissorCount" 1
+            &* setVkRef @"pScissors" scissorRect
 
-    rasterizer :: VkPipelineRasterizationStateCreateInfo
-    rasterizer = createVk @VkPipelineRasterizationStateCreateInfo
-      $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"depthClampEnable" VK_FALSE
-      &* set @"rasterizerDiscardEnable" VK_FALSE
-      &* set @"polygonMode" VK_POLYGON_MODE_FILL
-      &* set @"cullMode" VK_CULL_MODE_BACK_BIT
-      &* set @"frontFace" VK_FRONT_FACE_CLOCKWISE
-      &* set @"depthBiasEnable" VK_FALSE
-      &* set @"depthBiasConstantFactor" 0
-      &* set @"depthBiasClamp" 0
-      &* set @"depthBiasSlopeFactor" 0
-      &* set @"lineWidth" 1.0
+        rasterizer :: VkPipelineRasterizationStateCreateInfo
+        rasterizer = createVk @VkPipelineRasterizationStateCreateInfo
+            $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"depthClampEnable" VK_FALSE
+            &* set @"rasterizerDiscardEnable" VK_FALSE
+            &* set @"polygonMode" VK_POLYGON_MODE_FILL
+            &* set @"cullMode" VK_CULL_MODE_BACK_BIT
+            &* set @"frontFace" VK_FRONT_FACE_CLOCKWISE
+            &* set @"depthBiasEnable" VK_FALSE
+            &* set @"depthBiasConstantFactor" 0
+            &* set @"depthBiasClamp" 0
+            &* set @"depthBiasSlopeFactor" 0
+            &* set @"lineWidth" 1.0
 
-    multisampling :: VkPipelineMultisampleStateCreateInfo
-    multisampling = createVk @VkPipelineMultisampleStateCreateInfo
-      $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"sampleShadingEnable" VK_FALSE
-      &* set @"rasterizationSamples" VK_SAMPLE_COUNT_1_BIT
-      &* set @"minSampleShading" 1.0
-      &* set @"pSampleMask" VK_NULL
-      &* set @"alphaToCoverageEnable" VK_FALSE
-      &* set @"alphaToOneEnable" VK_FALSE
+        multisampling :: VkPipelineMultisampleStateCreateInfo
+        multisampling = createVk @VkPipelineMultisampleStateCreateInfo
+            $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"sampleShadingEnable" VK_FALSE
+            &* set @"rasterizationSamples" VK_SAMPLE_COUNT_1_BIT
+            &* set @"minSampleShading" 1.0
+            &* set @"pSampleMask" VK_NULL
+            &* set @"alphaToCoverageEnable" VK_FALSE
+            &* set @"alphaToOneEnable" VK_FALSE
 
-    colorBlendAttachment :: VkPipelineColorBlendAttachmentState
-    colorBlendAttachment = createVk @VkPipelineColorBlendAttachmentState
-      $  set @"colorWriteMask" (VK_COLOR_COMPONENT_R_BIT .|. VK_COLOR_COMPONENT_G_BIT .|. VK_COLOR_COMPONENT_B_BIT .|. VK_COLOR_COMPONENT_A_BIT)
-      &* set @"blendEnable" VK_FALSE
-      &* set @"srcColorBlendFactor" VK_BLEND_FACTOR_ONE
-      &* set @"dstColorBlendFactor" VK_BLEND_FACTOR_ZERO
-      &* set @"colorBlendOp" VK_BLEND_OP_ADD
-      &* set @"srcAlphaBlendFactor" VK_BLEND_FACTOR_ONE
-      &* set @"dstAlphaBlendFactor" VK_BLEND_FACTOR_ZERO
-      &* set @"alphaBlendOp" VK_BLEND_OP_ADD
+        colorBlendAttachment :: VkPipelineColorBlendAttachmentState
+        colorBlendAttachment = createVk @VkPipelineColorBlendAttachmentState
+            $  set @"colorWriteMask" (VK_COLOR_COMPONENT_R_BIT .|. VK_COLOR_COMPONENT_G_BIT .|. VK_COLOR_COMPONENT_B_BIT .|. VK_COLOR_COMPONENT_A_BIT)
+            &* set @"blendEnable" VK_FALSE
+            &* set @"srcColorBlendFactor" VK_BLEND_FACTOR_ONE
+            &* set @"dstColorBlendFactor" VK_BLEND_FACTOR_ZERO
+            &* set @"colorBlendOp" VK_BLEND_OP_ADD
+            &* set @"srcAlphaBlendFactor" VK_BLEND_FACTOR_ONE
+            &* set @"dstAlphaBlendFactor" VK_BLEND_FACTOR_ZERO
+            &* set @"alphaBlendOp" VK_BLEND_OP_ADD
 
-    colorBlending :: VkPipelineColorBlendStateCreateInfo
-    colorBlending = createVk @VkPipelineColorBlendStateCreateInfo
-      $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"logicOpEnable" VK_FALSE
-      &* set @"logicOp" VK_LOGIC_OP_COPY
-      &* set @"attachmentCount" 1
-      &* setVkRef @"pAttachments" colorBlendAttachment
-      &* setAt @"blendConstants" @0 0.0
-      &* setAt @"blendConstants" @1 0.0
-      &* setAt @"blendConstants" @2 0.0
-      &* setAt @"blendConstants" @3 0.0
+        colorBlending :: VkPipelineColorBlendStateCreateInfo
+        colorBlending = createVk @VkPipelineColorBlendStateCreateInfo
+            $  set @"sType" VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"logicOpEnable" VK_FALSE
+            &* set @"logicOp" VK_LOGIC_OP_COPY
+            &* set @"attachmentCount" 1
+            &* setVkRef @"pAttachments" colorBlendAttachment
+            &* setAt @"blendConstants" @0 0.0
+            &* setAt @"blendConstants" @1 0.0
+            &* setAt @"blendConstants" @2 0.0
+            &* setAt @"blendConstants" @3 0.0
 
-    getGraphicsPipelineCreateInfo :: Ptr VkPipelineShaderStageCreateInfo -> Word32 -> VkGraphicsPipelineCreateInfo
-    getGraphicsPipelineCreateInfo shaderStageInfosPtr shaderStageInfoCount = createVk @VkGraphicsPipelineCreateInfo
-      $  set @"sType" VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO
-      &* set @"pNext" VK_NULL
-      &* set @"flags" VK_ZERO_FLAGS
-      &* set @"stageCount" shaderStageInfoCount
-      &* set @"pStages" shaderStageInfosPtr
-      &* setVkRef @"pVertexInputState" vertexInputInfo
-      &* setVkRef @"pInputAssemblyState" inputAssembly
-      &* set @"pTessellationState" VK_NULL
-      &* setVkRef @"pViewportState" viewPortState
-      &* setVkRef @"pRasterizationState" rasterizer
-      &* setVkRef @"pMultisampleState" multisampling
-      &* set @"pDepthStencilState" VK_NULL
-      &* setVkRef @"pColorBlendState" colorBlending
-      &* set @"pDynamicState" VK_NULL
-      &* set @"layout" pipelineLayout
-      &* set @"renderPass" renderPass
-      &* set @"subpass" 0
-      &* set @"basePipelineHandle" VK_NULL_HANDLE
-      &* set @"basePipelineIndex" (-1)
+        getGraphicsPipelineCreateInfo :: Ptr VkPipelineShaderStageCreateInfo -> Word32 -> VkGraphicsPipelineCreateInfo
+        getGraphicsPipelineCreateInfo shaderStageInfosPtr shaderStageInfoCount = createVk @VkGraphicsPipelineCreateInfo
+            $  set @"sType" VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO
+            &* set @"pNext" VK_NULL
+            &* set @"flags" VK_ZERO_FLAGS
+            &* set @"stageCount" shaderStageInfoCount
+            &* set @"pStages" shaderStageInfosPtr
+            &* setVkRef @"pVertexInputState" vertexInputInfo
+            &* setVkRef @"pInputAssemblyState" inputAssembly
+            &* set @"pTessellationState" VK_NULL
+            &* setVkRef @"pViewportState" viewPortState
+            &* setVkRef @"pRasterizationState" rasterizer
+            &* setVkRef @"pMultisampleState" multisampling
+            &* set @"pDepthStencilState" VK_NULL
+            &* setVkRef @"pColorBlendState" colorBlending
+            &* set @"pDynamicState" VK_NULL
+            &* set @"layout" pipelineLayout
+            &* set @"renderPass" renderPass
+            &* set @"subpass" 0
+            &* set @"basePipelineHandle" VK_NULL_HANDLE
+            &* set @"basePipelineIndex" (-1)
 
-  logInfo $ "Create Pipeline: " ++ show imageExtent
-  shaderStageInfosPtr <- newArray shaderStageInfos
-  let graphicsPipelineCreateInfo = getGraphicsPipelineCreateInfo shaderStageInfosPtr (fromIntegral $ length shaderStageInfos)
-  createGraphicsPipelinesFunc <- vkGetDeviceProc @VkCreateGraphicsPipelines device
+    logInfo $ "Create Pipeline: " ++ show imageExtent
+    shaderStageInfosPtr <- newArray shaderStageInfos
+    let graphicsPipelineCreateInfo = getGraphicsPipelineCreateInfo shaderStageInfosPtr (fromIntegral $ length shaderStageInfos)
+    createGraphicsPipelinesFunc <- vkGetDeviceProc @VkCreateGraphicsPipelines device
 
-  graphicsPipeline <- alloca $ \graphicsPipelinePtr -> do
-      result <- createGraphicsPipelinesFunc device VK_NULL_HANDLE 1 (unsafePtr graphicsPipelineCreateInfo) VK_NULL graphicsPipelinePtr
-      validationVK result "vkCreatePipelines failed!"
-      peek graphicsPipelinePtr
+    graphicsPipeline <- alloca $ \graphicsPipelinePtr -> do
+        result <- createGraphicsPipelinesFunc device VK_NULL_HANDLE 1 (unsafePtr graphicsPipelineCreateInfo) VK_NULL graphicsPipelinePtr
+        validationVK result "vkCreatePipelines failed!"
+        peek graphicsPipelinePtr
 
-  touchVkData graphicsPipelineCreateInfo
-  free shaderStageInfosPtr
+    touchVkData graphicsPipelineCreateInfo
+    free shaderStageInfosPtr
 
-  return GraphicsPipelineData
-    { _vertexShaderCreateInfo = vertexShaderCreateInfo
-    , _fragmentShaderCreateInfo = fragmentShaderCreateInfo
-    , _pipelineLayout = pipelineLayout
-    , _pipeline = graphicsPipeline }
+    return GraphicsPipelineData
+      { _vertexShaderCreateInfo = vertexShaderCreateInfo
+      , _fragmentShaderCreateInfo = fragmentShaderCreateInfo
+      , _pipelineLayout = pipelineLayout
+      , _pipeline = graphicsPipeline }
 
 
 destroyGraphicsPipeline :: VkDevice -> GraphicsPipelineData -> IO ()
