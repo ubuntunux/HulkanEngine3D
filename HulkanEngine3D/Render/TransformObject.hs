f40e9f7231c91941a7e8050fdac54d064f0c8f78
ubuntunux <ubuntunux@gmail.com>
2020-02-29 04:53:10 +0900

    update

--- HulkanEngine3D/Render/TransformObject.hs
+++ HulkanEngine3D/Render/TransformObject.hs
@@ -5,21 +5,35 @@
 
 module HulkanEngine3D.Render.TransformObject
   ( TransformObject (..)
+  , TransformObjectInterface (..)
   ) where
 
 
 import GHC.Generics (Generic)
 import Numeric.DataFrame
-import Numeric.Dimensions
 
-import HulkanEngine3D.Utilities.System
-import HulkanEngine3D.Utilities.Logger
+import HulkanEngine3D.Utilities.Math
 
 
 data TransformObject = TransformObject
     { _position :: Vec3f
     , _rotation :: Vec3f
     , _scale :: Vec3f
+    , _matrix :: Mat44f
+    , _invMatrix :: Mat44f
     } deriving (Show, Generic)
 
 instance PrimBytes TransformObject
+
+
+class TransformObjectInterface a where
+    getDefailtTransformObject :: a -> a
+
+
+instance TransformObjectInterface TransformObject where
+    getDefailtTransformObject transformObject = TransformObject
+        { _position = float3_zero
+        , _rotation = float3_zero
+        , _scale = fromScalar 1
+        , _matrix = matrix4_indentity
+        , _invMatrix = matrix4_indentity }
--- HulkanEngine3D/Utilities/Math.hs
+++ HulkanEngine3D/Utilities/Math.hs
@@ -8,15 +8,44 @@
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeOperators       #-}
 
-module HulkanEngine3D.Utilities.Math
-  ( dataFrameLength
-  ) where
+module HulkanEngine3D.Utilities.Math where
 
 import Numeric.DataFrame
 import Numeric.Dimensions
 
 -- | Get number of points in a vector
 dataFrameLength :: DataFrame t (xns :: [XNat]) -> Word
-dataFrameLength (XFrame (_ :: DataFrame t ns)) = case dims @ns of
+dataFrameLength (XFrame (vector :: DataFrame t ns)) = case dims @ns of
     n :* _ -> dimVal n
     U      -> 1
+
+float_zero :: Scalar Float
+float_zero = S 0
+
+float2_zero :: Vec2f
+float2_zero = vec2 0 0
+
+float3_zero :: Vec3f
+float3_zero = vec3 0 0 0
+
+float4_zero :: Vec4f
+float4_zero = vec4 0 0 0 0
+
+getFloat :: Float -> Scalar Float
+getFloat v = S v
+
+getFloat2 :: Float -> Vec2f
+getFloat2 v = fromScalar (S v)
+
+getFloat3 :: Float -> Vec3f
+getFloat3 v = fromScalar (S v)
+
+getFloat4 :: Float -> Vec4f
+getFloat4 v = fromScalar (S v)
+
+matrix4_indentity :: Mat44f
+matrix4_indentity =
+    DF4 (DF4 1 0 0 0)
+        (DF4 0 1 0 0)
+        (DF4 0 0 1 0)
+        (DF4 0 0 0 1)
