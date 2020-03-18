690771703c2c85915c9c1bebc1cae985ad9de78a
ubuntunux <ubuntunux@gmail.com>
2020-03-19 01:06:45 +0900

    update

--- HulkanEngine3D/Render/Mesh.hs
+++ HulkanEngine3D/Render/Mesh.hs
@@ -1,44 +1,58 @@
-{-# LANGUAGE StandaloneDeriving  #-}
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE NegativeLiterals    #-}
-{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE RecordWildCards        #-}
+{-# LANGUAGE NegativeLiterals       #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE TypeSynonymInstances   #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE InstanceSigs           #-}
 
 module HulkanEngine3D.Render.Mesh
     ( MeshData (..)
     , MeshInterface (..)
+    , GeometryBufferDataList
     ) where
 
+import Control.Monad
 import Data.IORef
-import Data.Text
+import qualified Data.Text as Text
+import Control.Monad.Primitive
 import qualified Data.Vector as Vector
 import qualified Data.Vector.Mutable as MVector
 
 import HulkanEngine3D.Vulkan.GeometryBuffer
 import HulkanEngine3D.Utilities.System
 
+type GeometryBufferDataList = MVector.IOVector GeometryBufferData
+
+instance Show GeometryBufferDataList where
+    show a = show . MVector.length $ a
 
 data MeshData = MeshData
-    { _name :: IORef Text
+    { _name :: IORef Text.Text
     , _boundBox :: Bool
     , _skeletonDatas :: [Bool]
     , _animationDatas :: [Bool]
-    --, _geometryBufferDatas :: MVector.MVector () GeometryBufferData
+    , _geometryBufferDatas :: GeometryBufferDataList
     } deriving (Show)
 
 class MeshInterface a where
-    newMeshData :: Text -> IO a
+    newMeshData :: Text.Text -> [GeometryBufferData] -> IO a
     updateMeshData :: a -> IO ()
 
 instance MeshInterface MeshData where
-    newMeshData meshName = do
+    newMeshData :: Text.Text -> [GeometryBufferData] -> IO MeshData
+    newMeshData meshName geometryBufferDatas = do
         nameRef <- newIORef meshName
+        geometryBufferDataList <- MVector.new (length geometryBufferDatas)
+        forM_ (zip [0..] geometryBufferDatas) $ \(index, geometryBufferData) ->
+            MVector.write geometryBufferDataList index geometryBufferData
         return MeshData
             { _name = nameRef
             , _boundBox = False
             , _skeletonDatas = []
             , _animationDatas = []
+            , _geometryBufferDatas = geometryBufferDataList
             }
 
+    updateMeshData :: MeshData -> IO ()
     updateMeshData meshData = return ()
-
