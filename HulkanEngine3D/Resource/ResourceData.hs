7e1ccc55aa7b6c47d5126077e73ecc8a47475a68
ubuntunux <ubuntunux@gmail.com>
2020-06-20 04:30:42 +0900

    update

--- HulkanEngine3D/Resource/ResourceData.hs
+++ HulkanEngine3D/Resource/ResourceData.hs
@@ -5,9 +5,28 @@ module HulkanEngine3D.Resource.ResourceData where
 import qualified Data.Text as Text
 --import qualified Data.Time as Time
 
+    { _metaDataMap :: MetaDataMap
+    , _meshDataMap :: MeshDataMap
+    , _modelDataMap :: ModelDataMap
+    , _textureDataMap :: TextureDataMap
+    , _frameBufferDataMap :: FrameBufferDataMap
+    , _renderPassDataMap :: RenderPassDataMap
+    , _materialInstanceDataMap :: MaterialInstanceDataMap
+    , _descriptorDataMap :: DescriptorDataMap
+
+data ResourceDataType = ResourceDataType_Mesh
+                      | ResourceDataType_Model
+                      | ResourceDataType_Texture
+                      | ResourceDataType_FrameBuffer
+                      | ResourceDataType_RenderPass
+                      | ResourceDataType_MaterialInstance
+                      | ResourceDataType_Descriptor
+                      deriving (Eq, Show)
+
 data MetaData = MetaData
     { _isEngineResource :: Bool
     , _metaFilePath :: Bool
+    , _resourceDataType :: ResourceDataType
     , _resourceVersion :: Int
     , _resourceFilePath :: FilePath
     , _resourceModifyTime :: Text.Text
