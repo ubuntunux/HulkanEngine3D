09d6f04dfb42f7f30379ef6e0c5b52d5f5a6c16f
ubuntunux <ubuntunux@gmail.com>
2020-07-09 03:04:22 +0900

    update

--- HulkanEngine3D/Resource/RenderPassCreateInfo/RenderDefault.hs
+++ HulkanEngine3D/Resource/RenderPassCreateInfo/RenderDefault.hs
@@ -68,7 +68,7 @@ getRenderPassDataCreateInfo rendererData = do
                 { _attachmentImageFormat = format
                 , _attachmentImageSamples = sampleCount
                 , _attachmentLoadOperation = VK_ATTACHMENT_LOAD_OP_CLEAR
-                , _attachmentStoreOperation = VK_ATTACHMENT_STORE_OP_DONT_CARE
+                , _attachmentStoreOperation = VK_ATTACHMENT_STORE_OP_STORE
                 , _attachmentFinalLayout = VK_IMAGE_LAYOUT_GENERAL
                 , _attachmentReferenceLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL
                 } | format <- _frameBufferDepthAttachmentFormats frameBufferDataCreateInfo
