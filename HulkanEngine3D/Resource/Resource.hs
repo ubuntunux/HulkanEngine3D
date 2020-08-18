dbba2e20eee8b9526b959e613feb3825ef2ba669
ubuntunux <ubuntunux@gmail.com>
2020-08-19 03:54:52 +0900

    update

--- HulkanEngine3D/Resource/Resource.hs
+++ HulkanEngine3D/Resource/Resource.hs
@@ -502,8 +502,8 @@ instance ResourceInterface Resources where
                     Just (Aeson.Array pipelineCreateInfoArray) = HashMap.lookup "pipelines" materialCreateInfo
                     Aeson.Object materialParameterMap = HashMap.lookupDefault (Aeson.Object HashMap.empty) "material_parameters" materialCreateInfo
                 renderPassPipelineDataList <- forM pipelineCreateInfoArray $ \(Aeson.Object pipelineCreateInfo) -> do
-                    let Just (Aeson.String renderPassDataName) = HashMap.lookup "render_pass_name" pipelineCreateInfo
-                        Just (Aeson.String pipelineDataName) = HashMap.lookup "pipeline_name" pipelineCreateInfo
+                    let Just (Aeson.String renderPassDataName) = HashMap.lookup "renderPass" pipelineCreateInfo
+                        Just (Aeson.String pipelineDataName) = HashMap.lookup "pipleline" pipelineCreateInfo
                     getRenderPassPipelineData resources (renderPassDataName, pipelineDataName)
                 material <- Material.createMaterial materialName (Vector.toList renderPassPipelineDataList) materialParameterMap
                 HashTable.insert (_materialDataMap resources) materialName material
