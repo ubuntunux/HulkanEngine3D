d09887a774ba1c6608f324029b54f4cfd88015a6
ubuntunux <ubuntunux@gmail.com>
2020-09-01 02:21:01 +0900

    update

---
+++ HulkanEngine3D/Resource/ColladaLoader.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE InstanceSigs           #-}
+{-# LANGUAGE RecordWildCards        #-}
+{-# LANGUAGE NegativeLiterals       #-}
+{-# LANGUAGE OverloadedStrings      #-}
+
+module HulkanEngine3D.Resource.ColladaLoader where
