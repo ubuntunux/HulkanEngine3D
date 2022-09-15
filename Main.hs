570d1d8f3e75ffa9f67d679a7081330e9e6c48bb
ubuntu <ubuntunux@gmail.com>
2022-09-15 23:12:50 +0900

    fix windows build error

--- Main.hs
+++ Main.hs
@@ -3,18 +3,11 @@ module Main
   ) where
 
 import Data.IORef
-import Control.Concurrent (forkIO)
-
-import Web.Browser
-
 import HulkanEngine3D.Application.Application
 import HulkanEngine3D.Application.Command
-import HulkanEngine3D.Application.Editor
 
 main::IO()
 main = do
-    openBrowser "http://127.0.0.1:8023/"
     commandToEditor <- newIORef Command_None
     commandToApp <- newIORef Command_None
-    forkIO $ runEditor commandToEditor commandToApp
     runApplication commandToEditor commandToApp
